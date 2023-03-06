76b6598c051b1228c1fa18b850508cf1f081d94f
ACGMN <ootopoo@vip.qq.com>
2023-03-06 18:13:56 +0800

    Update
    Signed-off-by: ACGMN <ootopoo@vip.qq.com>

--- rust-syntax-stable/src/test_drop.rs
+++ rust-syntax-stable/src/test_drop.rs
@@ -7,14 +7,17 @@ impl Drop for Foo {
     }
 }
 
-/*impl Clone for Foo {
+impl Clone for Foo {
     fn clone(&self) -> Self {
-        todo!()
+        Foo
     }
 }
 
-impl Copy for Foo {}*/
+// impl Copy for Foo {}
 
+fn my_drop(foo: Foo) {
+    println!("still not dropped");
+}
 
 #[test]
 fn test_drop() {
@@ -22,8 +25,22 @@ fn test_drop() {
     println!("test_drop");
     let foo = Foo;
     println!("{:?}", foo);
-    //Only use is to move foo in order to call drop of foo
+    //Only use is to move foo in order to destruct foo once and only once at the end of core::mem::drop
     //If Copy trait is implemented for Foo, foo won't be moved
     core::mem::drop(foo);
     // println!("{:?}", foo);
+    println!("start");
+    let foo = Foo;
+    my_drop(foo);
+    println!("end");
+    let foo = Foo;
+    let foo2 = foo;
+    println!("only dropped once");
+}
+
+#[test]
+fn test_drop2() {
+    let foo = Foo;
+    let foo = Foo;
+    println!("drop shadowed variable also");
 }
